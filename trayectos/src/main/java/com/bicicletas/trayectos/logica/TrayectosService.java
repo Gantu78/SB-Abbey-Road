e33c4aa885f6428d9c17a56da1553a0b1d723988
Danielhoyos06 <danielhoyos2006@gmail.com>
2025-04-30 04:51:59 +0000

    Caso de uso numero 4 realizado

--- trayectos/src/main/java/com/bicicletas/trayectos/logica/TrayectosService.java
+++ trayectos/src/main/java/com/bicicletas/trayectos/logica/TrayectosService.java
@@ -1,6 +1,8 @@
 package com.bicicletas.trayectos.logica;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -105,7 +107,97 @@ public class TrayectosService {
         return trayectoActual.getId();
 
     }
+// CU003 - Finalizar Trayecto
+    // 1. Ingresa el id del trayecto en curso
+    // 4. Ingresa la longitud y la latitud de la ubicación actual
+    @Transactional(value = TxType.REQUIRED)    
+    public UUID finalizarTrayecto(UUID idTrayecto, Double longitud, Double latitud) 
+        throws Exception
+    {
 
+        // 2. verifica que exista un trayecto con ese id 
+
+        // busca el trayecto usando el parámetro idTrayecto
+        Optional<Trayecto> optTrayecto = trayectos.findById(idTrayecto);
+
+        // lanza una excepción si no existe el trayecto
+        if (optTrayecto.isEmpty()) {
+            throw new Exception("No existe un trayecto con el id " + idTrayecto);
+        }
+
+        // 3. verifica que el trayecto esté activo 
+
+        // obtiene el objeto con el trayecto actual
+        Trayecto trayectoActual = optTrayecto.get();
+
+        // lanza una excepción si el trayecto no está en proceso
+        if (! trayectoActual.isEnProceso()) {
+            throw new Exception("El trayecto con el id " + idTrayecto + "no está activo");
+        }
+
+        // 5. Determina fecha y hora 
+
+        // obtiene un objeto Date con la fecha actual
+        LocalDateTime fechaActual = LocalDateTime.now();
+
+        // 6. Agrega  una nueva ubicación con fecha y hora actual 
+        //    y la longitud y latitud de la ubicación al trayecto en curso 
+
+        // Crea una nueva ubicación
+        Ubicacion ubicacion = new Ubicacion();
+        ubicacion.setFechaHora(fechaActual);
+        ubicacion.setLongitud(longitud);
+        ubicacion.setLatitud(latitud);
+        ubicacion.setTrayecto(trayectoActual);
+
+        // guarda la ubicación
+        ubicacion = ubicaciones.save(ubicacion);
+
+        // asigna la fecha y hora final al trayecto
+        trayectoActual.setFechaHoraFin(fechaActual);
+
+        // agrega la ubicación al trayecto
+        trayectoActual.getUbicaciones().add(ubicacion);
+
+        // 7. Calcula la duración del trayecto 
+        Long duracion = Duration.between(
+                                trayectoActual.getFechaHoraInicio(), 
+                                trayectoActual.getFechaHoraFin())
+                            .toSeconds();
+
+
+        // 8. Actualiza el trayecto grabando la hora final del trayecto, 
+        //    la duración y cambiando el estado a inactivo 
+
+        trayectoActual.setDuracion(duracion);
+        trayectoActual.setEnProceso(false);
+        trayectoActual = trayectos.save(trayectoActual);
+
+        // returna el id
+        return trayectoActual.getId();
+
+    }
+// CU004 - Consultar estadísticas del trayecto
+    // 1. Ingresa el id del trayecto a revisar
+    @Transactional(value = TxType.SUPPORTS)
+    public Trayecto consultarTrayecto(UUID idTrayecto)
+        throws Exception {
+
+        // 2. verifica que exista un trayecto con ese id
+        Trayecto trayecto = trayectos.findById(idTrayecto).orElse(null);
+
+        if (trayecto == null) {
+            throw new Exception("No existe trayecto con el id " + idTrayecto);
+        }
+
+        // 3. Muestra la información de la hora de inicio, la longitud y latitud 
+        //    de la ubicación inicial, la longitud y latitud de todas las ubicaciones 
+        //    de ese trayecto, la distancia recorrida, la hora final y la longitud y 
+        //    latitud de la última ubicación
+         
+        return trayecto;
+
+    }
 
 }    
 
